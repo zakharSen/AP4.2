63db01263557b10570b3900b7b3b7d61d38e5d02
zakharSen <139217440+zakharSen@users.noreply.github.com>
2023-10-11 16:33:18 +0300

    Add project files.

---
+++ AP4.2/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = x;
+		if (x <= -1)
+			B = log(fabs(cos(x * 5))) + exp(1 / x + x);
+		else
+			if (-1 < x && x < 0.4)
+				B = sqrt(cbrt(2 - x)) - tan(x);
+			else
+				B = sin(x * 5) - sqrt(fabs(1 - x));
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
